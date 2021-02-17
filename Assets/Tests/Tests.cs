58754fd247c001886188ec84426ea7510251b47a
sebas77 <ssense@tiscali.it>
2021-02-17 11:16:41 +0000

    Working on removing allocations during building of entities if preallocate is used

--- Assets/Tests/Tests.cs
+++ Assets/Tests/Tests.cs
@@ -1,52 +1,95 @@
-﻿using System.Collections;
+﻿#warning to run the tests, open the Unity Test Runner
+
+using System;
 using NUnit.Framework;
 using Svelto.ECS;
 using Svelto.ECS.Schedulers;
+using UnityEngine.TestTools.Constraints;
+using Is = NUnit.Framework.Is;
 
-///Note: these tests are only meant to test the code on several platforms. The extensive test coveage of Svelto.ECS
+///Note: these tests are only meant to test the code on unity platforms. The extensive test coveage of Svelto.ECS
 /// is found at https://github.com/sebas77/Svelto.ECS.Tests
 namespace Tests
 {
     [TestFixture]
     public class Tests
     {
-        EnginesRoot _enginesRoot;
+        EnginesRoot                       _enginesRoot;
         SimpleEntitiesSubmissionScheduler _simpleEntitiesSubmissionScheduler;
-        readonly ExclusiveGroup @group = new ExclusiveGroup();
-        TestEngine _testEngine;
+        readonly ExclusiveGroup           @group = new ExclusiveGroup();
+        TestEngine                        _testEngine;
 
         [SetUp]
         public void Setup()
         {
             _simpleEntitiesSubmissionScheduler = new SimpleEntitiesSubmissionScheduler();
-            _enginesRoot = new EnginesRoot(_simpleEntitiesSubmissionScheduler);
-            _testEngine = new TestEngine(@group);
+            _enginesRoot                       = new EnginesRoot(_simpleEntitiesSubmissionScheduler);
+            _testEngine                        = new TestEngine(@group);
             _enginesRoot.AddEngine(_testEngine);
         }
-        
+
         // A Test behaves as an ordinary method
         [Test]
-        public void SimplePasses()
+        public void Factory()
         {
             var init = _enginesRoot.GenerateEntityFactory().BuildEntity<TestEntityDescriptor>(new EGID(0, group));
-            init.Init(new UnmanagedComponent() {test = 2});
+            init.Init(new UnmanagedComponent()
+            {
+                test = 2
+            });
             _simpleEntitiesSubmissionScheduler.SubmitEntities();
 
             Assert.DoesNotThrow(() => _testEngine.Test());
-            
+
             _enginesRoot.Dispose();
         }
+
+#if PROFILE_SVELTO
+        [Test]
+        public void Preallocation()
+        {
+            void BuildEntity(IEntityFactory entityFactory)
+            {
+                var init = entityFactory.BuildEntity<TestEntityDescriptor>(new EGID(0, @group));
+                init.Init(new UnmanagedComponent()
+                {
+                    test = 2
+                });
+            }
+
+            var generateEntityFactory = _enginesRoot.GenerateEntityFactory();
+            generateEntityFactory.PreallocateEntitySpace<TestEntityDescriptor>(group, 100);
+            generateEntityFactory.BuildEntity<TestEntityDescriptor>(new EGID(0, group));
+            
+            _simpleEntitiesSubmissionScheduler.SubmitEntities(); //warm up method
+            
+            _simpleEntitiesSubmissionScheduler = new SimpleEntitiesSubmissionScheduler();
+            _enginesRoot                       = new EnginesRoot(_simpleEntitiesSubmissionScheduler);
+            
+            generateEntityFactory              = _enginesRoot.GenerateEntityFactory();
+            generateEntityFactory.PreallocateEntitySpace<TestEntityDescriptor>(group, 100);
+           
+            Assert.That(() =>  BuildEntity(generateEntityFactory), Is.Not.AllocatingGCMemory());
+            Assert.That(_simpleEntitiesSubmissionScheduler.SubmitEntities, Is.Not.AllocatingGCMemory());
+
+            _enginesRoot.Dispose();
+        }
+#endif
         
         // A Test behaves as an ordinary method
         [Test]
         public void NativeFactory()
         {
-            var init = _enginesRoot.GenerateEntityFactory().ToNative<TestEntityDescriptor>("test").BuildEntity(new EGID(0, group), 0);
-            init.Init(new UnmanagedComponent() {test = 2});
+            var init = _enginesRoot.GenerateEntityFactory().ToNative<TestEntityDescriptor>("test")
+                                   .BuildEntity(new EGID(0, group), 0);
+            init.Init(new UnmanagedComponent()
+            {
+                test = 2
+            });
             _simpleEntitiesSubmissionScheduler.SubmitEntities();
 
             Assert.DoesNotThrow(() => _testEngine.Test());
-            
+
             _enginesRoot.Dispose();
         }
     }
@@ -55,7 +98,7 @@ namespace Tests
 
     public struct UnmanagedComponent : IEntityComponent, INeedEGID
     {
-        public int test;
+        public int  test;
         public EGID ID { get; set; }
     }
 
@@ -65,17 +108,19 @@ namespace Tests
         public TestEngine(ExclusiveGroup @group) { this.@group = @group; }
         public EntitiesDB entitiesDB { get; set; }
 
-        public void Ready()
-        {
-        }
+        public void Ready() { }
 
         public void Test()
         {
             var (buffer, count) = entitiesDB.QueryEntities<UnmanagedComponent>(@group);
 
+            if (count == 0)
+                throw new Exception();
+            
             for (int i = 0; i < count; ++i)
             {
-                buffer[i].test = 1;
+                if (buffer[i].test != 2)
+                    throw new Exception();
             }
         }
     }
